61df765b75b5db3976a6c0a3f3e015bf817c3051
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2019-01-04 09:32:50 -0500

    Add +x to bin output

--- Build.hx
+++ Build.hx
@@ -2,7 +2,13 @@ using haxe.io.Path;
 
 class Build {
   static function main() {
+    var cwd = Sys.getCwd();
     Sys.command('haxe', ['haxeshim.hxml']);
+    Sys.command('chmod', ['+x', '${cwd}bin/haxelibshim.js']);
+    Sys.command('chmod', ['+x', '${cwd}bin/haxeshim.js']);
+    Sys.command('chmod', ['+x', '${cwd}bin/lix.js']);
+    Sys.command('chmod', ['+x', '${cwd}bin/nekoshim.js']);
+    Sys.command('chmod', ['+x', '${cwd}bin/postinstall.js']);
     Sys.command('haxe', ['lix.cli.hxml']);
     for (file in sys.FileSystem.readDirectory('bin')) {
       if(file.extension() == 'js') {
